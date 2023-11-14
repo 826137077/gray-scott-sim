906b1dee26d230767bf62a27927518dcc8611e90
826137077 <49941764+826137077@users.noreply.github.com>
2023-11-14 20:07:12 +0000

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 // gs_test.cpp
 #define TESTING
-#include "../gs.cpp" 
+#include "gs.cpp" 
 #include "acutest.h"
 
 
@@ -20,6 +20,7 @@ void test_same_size() {
     }
 }
 
+//Check that the simulation produces the mathematically correct answer when u = 0 and v = 0.
 void test_uvequalszero() {
 	
     std::srand(static_cast<unsigned int>(std::time(nullptr)));    
@@ -34,34 +35,20 @@ void test_uvequalszero() {
     // Main simulation loop
     for (int iteration = 0; iteration < numIterations; ++iteration) {
         simulateStep();
-        
-        // Periodically write to VTK file
-        if (iteration % outputInterval == 0) {
-            writeVTKFile(iteration);
-        }
     }
 
     // count the amount of pixels above threshold at end.
     double n = countElementsAboveThreshold(threshold);
-    double V = v[width-1][height-1];
-    double U = u[width-1][height-1];
-    std::cout << "Simulation completed: dU = " << dU << std::endl;
-    std::cout << "Simulation completed: dV = " << dV << std::endl;
+
     std::cout << "Simulation completed: P(v > threshold) = " << n << std::endl;
-	TEST_CHECK(n == 0.0);
-    //TEST_CHECK(dV == Dv*laplaceV);
-    //TEST_CHECK(dU == Du*laplaceU);
-    //TEST_MSG("Produced: %f", n);
-    //TEST_MSG("Produced_dU: %f", n);
-    TEST_MSG("Produced_dV: %f", dV);
-	//getchar();
-	//getchar();
+    TEST_CHECK(n == 0.0);
+    TEST_MSG("Produced n: %f", n);
 }
 
 
 TEST_LIST = {
     {"test_type", test_type},
-    { "test_size", test_same_size },
-    { "simulation_with_zero_initial_conditions", test_uvequalszero },
+    {"test_size", test_same_size },
+    {"simulation_with_zero_initial_conditions", test_uvequalszero },
     { NULL, NULL } 
 };
