eae16e216b7d9079a2e08c62672b4035bbed1f77
MrSaku11a <146456305+MrSaku11a@users.noreply.github.com>
2023-11-15 14:31:15 +0000

    Update gs_test.cpp

--- gs_test.cpp
+++ gs_test.cpp
@@ -45,10 +45,87 @@ void test_uvequalszero() {
     TEST_MSG("Produced n: %f", n);
 }
 
+void test_init() {
+    init();
+    // check the value of u and v when call the function "init()"
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y) {
+            TEST_CHECK(u[x][y] == 1.0);  
+            if (x >= 100 && x <= 200 && y >= 100 && y <= 150) {
+                TEST_CHECK(v[x][y] >= 0.0 && v[x][y] < 1.0);  // Random ~ U[0.0,1.0)
+            } else {
+                TEST_CHECK(v[x][y] == 0.0);  
+            }
+        }
+    }
+}
+
+void test_writeVTKFile(){
+    // up to the tester, wo just set 10 for one VTKfile
+    int testIteration = 10;
+
+    // call func
+    writeVTKFile(testIteration);
+
+    // the target file name
+    std::stringstream expected_filename;
+    expected_filename << "output_" << testIteration << ".vtk";
+
+    // open the file
+    std::ifstream vtkFile(expected_filename.str());
+    
+    // check open
+    TEST_CHECK(vtkFile.good());
+
+    // read the file
+    std::string line;
+
+    // check the first two line of the file
+    std::getline(vtkFile, line);
+    TEST_CHECK(line == "# vtk DataFile Version 3.0");
+    std::getline(vtkFile, line);
+    TEST_CHECK(line == "Gray-Scott Simulation Output");
+    // close the file
+    vtkFile.close();
+
+    int remove_result = std::remove(expected_filename.str().c_str());
+
+    // check delete or not
+    TEST_CHECK(remove_result == 0);
+}
+
+void test_countElementsAboveThreshold(){
+     init();
+
+    v[50][50] = 0.45; // set value below
+    v[60][60] = 0.55; // set value above
+
+    // choose a thresholds
+    double test_threshold = 0.50;
+
+    // call func
+    double caluculate_result = countElementsAboveThreshold(test_threshold);
+
+    // verify the value
+    int expected_count = 0;
+    for (const auto& row : v) {
+        for (const double& element : row) {
+            if (element > test_threshold) {
+                expected_count++;
+            }
+        }
+    }
+    // check countElementsAboveThreshold output
+    TEST_CHECK(caluculate_result == (double)expected_count / (width * height));
+
+}
 
 TEST_LIST = {
     {"test_type", test_type},
     {"test_size", test_same_size },
-    {"simulation_with_zero_initial_conditions", test_uvequalszero },
+    {"test_uvequalszero", test_uvequalszero },
+    {"test_init", test_init },
+    {"test_writeVTKFile",test_writeVTKFile },
+    {"test_countElementsAboveThreshold",test_countElementsAboveThreshold },
     { NULL, NULL } 
 };
